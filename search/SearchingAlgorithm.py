44d106ebe44d18d31ca0686c90137ecc2b5f6f1e
vincent-haoy <53930677+vincent-haoy@users.noreply.github.com>
2021-03-31 06:13:16 +1100

    overlapping token

--- search/SearchingAlgorithm.py
+++ search/SearchingAlgorithm.py
@@ -9,7 +9,7 @@ def InitializeAGird(g):
     grid[r+4][q+4] = g
   return grid
   
-def BFS(src, target, visited, pred, dist, board):
+def BFS(src, target, restraint,visited, pred, dist, board):
 
   queue = []
   dist[src.getx()][src.gety()] = 0
@@ -21,23 +21,22 @@ def BFS(src, target, visited, pred, dist, board):
       neighbours = u.surrounding[i]
       neighbours_cards_len = len(neighbours.cards)
       if(neighbours_cards_len>0):
-        if(neighbours.cards[0].side == "block"):
-          visited[neighbours.getx()][neighbours.gety()] = True
-          continue
-        elif(HasAlly(neighbours.cards)):
+        if(HasAlly(neighbours.cards)):
           all_neighbours = neighbours.surrounding
           neighbours_surrounding_len = len(all_neighbours)
           for g in range(neighbours_surrounding_len):    
             if (visited[all_neighbours[g].getx()][all_neighbours[g].gety()] == False):
               visited[all_neighbours[g].getx()][all_neighbours[g].gety()] = True
-              if(all_neighbours[g].cards != [] and all_neighbours[g].cards[0].side == "block"):
+              if(all_neighbours[g].cards != [] and (all_neighbours[g].cards[0].side == "block" or all_neighbours[g].cards[0].role == restraint)):
                 continue
               dist[all_neighbours[g].getx()][all_neighbours[g].gety()]= dist[u.getx()][u.gety()]  + 1
               pred[all_neighbours[g].getx()][all_neighbours[g].gety()] = u
               queue.append(all_neighbours[g])
               if (all_neighbours[g].cards != [] and all_neighbours[g].cards[0].role == target and HasEnemy(all_neighbours[g].cards)):
                 return all_neighbours[g]
-
+        elif(neighbours.cards[0].side == "block" or neighbours.cards[0].role == restraint):
+          visited[neighbours.getx()][neighbours.gety()] = True
+          continue        
       if (visited[neighbours.getx()][neighbours.gety()] == False):
         visited[neighbours.getx()][neighbours.gety()] = True
         dist[neighbours.getx()][neighbours.gety()] = dist[u.getx()][u.gety()]  + 1
@@ -49,12 +48,13 @@ def BFS(src, target, visited, pred, dist, board):
 
 def RunDFSOnCards(src_card,board, turn):
     target = src_card.WantedOpponentRole()
+    restraint = src_card.ResistanceOpponentRole()
     visited = InitializeAGird(False)
     distance_to_source = InitializeAGird(999)
     predecessor = InitializeAGird(None)
     
     src = board.grid[src_card.getx()][src_card.gety()]
-    result = BFS(src, target, visited, predecessor, distance_to_source, board)
+    result = BFS(src, target,restraint, visited, predecessor, distance_to_source, board)
     if result is not None:
       nextpoint = findNextMove(predecessor, result, src).coordinate
       return nextpoint
@@ -75,20 +75,27 @@ def printMove(turn,current_card, nextpoint):
     print_swing(turn,current_card.coordinate[0],current_card.coordinate[1],nextpoint[0],nextpoint[1])
 
 def settlement(coordinate,board,upper,lower,move_dic):
-    upper_tokens = [x for x in board.grid[coordinate[0]+4][coordinate[1]+4].cards if x.getside() =="upper" ]
-    lower_tokens = [x for x in board.grid[coordinate[0]+4][coordinate[1]+4].cards if x.getside() =="lower" ]
-    for upper_token in upper_tokens:
-      for lower_token in lower_tokens:
-        if lower_token.role == upper_token.WantedOpponentRole():
-          lower.remove(lower_token)
-          board.deleOnboard(lower_token)
-
-    for upper_token in upper_tokens:
-      for lower_token in lower_tokens:
-        if lower_token.role == upper_token.ResistanceOpponentRole():
-          move_dic.pop(upper_token)
-          upper.remove(upper_token)
-          board.deleOnboard(upper_token)
+  upper_tokens = [x for x in board.grid[coordinate[0]+4][coordinate[1]+4].cards if x.getside() =="upper" ]
+  lower_tokens = [x for x in board.grid[coordinate[0]+4][coordinate[1]+4].cards if x.getside() =="lower" ]
+  hasp = False
+  hass = False
+  hasr = False
+  for i in upper_tokens + lower_tokens:
+    if i.role == 's':
+      hass = True
+    if i.role == 'p':
+      hasp = True
+    if i.role == 'r':
+      hasr = True
+  for g in upper_tokens:
+    if((hasp and g.ResistanceOpponentRole()=='p') or (hass and g.ResistanceOpponentRole()=='s') or (hasr and g.ResistanceOpponentRole() == 'r')):
+      move_dic.pop(g)
+      upper.remove(g)
+      board.deleOnboard(g)        
+  for z in lower_tokens:
+    if((hasp and z.ResistanceOpponentRole()=='p') or (hass and z.ResistanceOpponentRole()=='s') or (hasr and z.ResistanceOpponentRole() == 'r')):
+      lower.remove(z)
+      board.deleOnboard(z)
 
 def HasAlly(cards):
   for card in cards:
--- search/main.py
+++ search/main.py
@@ -17,7 +17,10 @@ import json
 from search.util import print_board, print_slide, print_swing
 from search.datastructure import *
 from search.SearchingAlgorithm import *
+import time
+import random
 def main():
+    start = time.time()
     try:
         with open(sys.argv[1]) as file:
             data = json.load(file)
@@ -36,14 +39,6 @@ def main():
     #stage1
     Upper = []
     Blocks = []
-    #stage2    
-    Upper_p = []
-    Upper_s = []
-    Upper_r = []
-    #stage3
-    Lower_p = []
-    Lower_s = []
-    Lower_r = []
     Lower = [] 
     board_dict ={
 
@@ -54,15 +49,7 @@ def main():
         Upper.append((MyNode(i[0],"upper",tuple((i[1],i[2])),game)))   
     for i in data['lower']:
         board_dict[tuple((i[1],i[2]))] = i[0]
-        Lower.append((MyNode(i[0],"lower",tuple((i[1],i[2])),game)))
-        """
-        if(i[0] == "p"):
-            Lower_p.append((MyNode(i[0],"lower",tuple((i[1],i[2])),game)))
-        elif(i[0] == "s"):
-            Lower_s.append((MyNode(i[0],"lower",tuple((i[1],i[2])),game)))
-        elif(i[0] == "r"):
-            Lower_r.append((MyNode(i[0],"lower",tuple((i[1],i[2])),game)))
-        """            
+        Lower.append((MyNode(i[0],"lower",tuple((i[1],i[2])),game)))         
     for i in data['block']: 
         board_dict[tuple((i[1],i[2]))] = 'b'
         Blocks.append((MyNode("b","block",tuple((i[1],i[2])),game)))
@@ -70,16 +57,98 @@ def main():
     move_dic = {
 
     }
+    no_targets = []
     turn = 1
-    print_board(board_dict)
+    #print_board(board_dict)
     while(len(Lower)>0):
+        p = 0
+        s = 0
+        r = 0
+        for lower_token in Lower:
+            if(lower_token.role == 'p'):
+                s+=1
+            elif(lower_token.role == 'r'):
+                p+=1
+            elif(lower_token.role == 's'):
+                r+=1
 
         for current_card in Upper:
             nextpoint = RunDFSOnCards(current_card, game, turn)
             if nextpoint is not None:
                 move_dic[current_card] = nextpoint
             else:
-                print("didnt find")
+                if current_card in move_dic.keys():
+                    move_dic.pop(current_card)
+                no_targets.append(current_card)
+                continue
+        
+        #if two tokens are move to the same location, we run random for either of them:
+
+        move_key = [key for key in move_dic.keys()]
+        move_value = [move_dic[key] for key in move_key]
+        same_spot = []
+        original_surrounding = []
+        for i in range(len(move_key)):
+            for j in range(i,len(move_key)):
+                if  (i != j ) and move_dic[move_key[i]] == move_dic[move_key[j]] and (move_key[i].role != move_key[j].role):
+                    same_spot.append(move_key[i])
+                    same_spot.append(move_key[j])            
+        for spot in same_spot:
+            available = []
+            for near in game.grid[spot.getx()][spot.gety()].surrounding:
+                if(len(near.cards) > 0):
+                    if(near.cards[0].side == 'block'):
+                        continue
+                    if(HasEnemy(near.cards) and spot.ResistanceOpponentRole() == near.cards[0].role):
+                        continue 
+                    available.append(near.coordinate)
+                else:
+                    available.append(near.coordinate)
+            original_surrounding.append(available)
+        possible_combination = []
+        if(len(same_spot) == 2):
+            possible_combination = [(a,b) for a in original_surrounding[0] for b in original_surrounding[1] if a !=b]
+        elif(len(same_spot) ==3):
+            possible_combination = [(a,b,c) for a in original_surrounding[0] for b in original_surrounding[1] for c in original_surrounding[2] if (a !=b and b!= c and c != a)]
+        comibination_len = len(possible_combination)
+        if(comibination_len > 0):
+            randn = random.randint(0,comibination_len-1)
+            move_dic[same_spot[0]] = possible_combination[randn][0]
+            move_dic[same_spot[1]] = possible_combination[randn][1]
+            if(len(same_spot)==3):
+                move_dic[same_spot[2]]= possible_combination[randn][2]
+        #if BFS cant find a target, move randomly
+        for no_target in no_targets:
+            # random move
+            acceptable_list = []
+            existing_key = [key for key in move_dic.keys()]
+            existing_value =[move_dic[key] for key in existing_key]
+            suicide_list = []
+            for spot in game.grid[no_target.getx()][no_target.gety()].surrounding:
+                if(len(spot.cards) > 0):
+                    if(spot.cards[0].role == 'block'):
+                        continue
+                    if(HasEnemy(spot.cards) and no_target.ResistanceOpponentRole() == spot.cards[0].role):
+                        suicide_list.append(spot.coordinate)
+                        continue            
+                else:
+                    acceptable_list.append(spot.coordinate)
+            
+            if((len(suicide_list) > 0) and ((no_target.role == 'p' and p == 0) or (no_target.role == 's' and s ==0) or (no_target.role == 'r' and r == 0))):
+                move_dic[no_target] = suicide_list [0]
+                no_targets.remove(no_target)
+                continue
+            
+            chooseable = list(set(acceptable_list) - set(existing_value))            
+            if len(chooseable) > 0:
+                n = random.randint(0,len(chooseable)-1)
+                move_dic[no_target] = chooseable[n]
+                no_targets.remove(no_target)
+                continue
+            else:
+                g = random.randint(0,len(acceptable_list)-1)
+                move_dic[no_target] = acceptable_list[g]
+                no_targets.remove(no_target)
                 continue
 
         for key in move_dic.keys():
@@ -89,4 +158,5 @@ def main():
         for key in list(move_dic.keys()):   
             settlement(move_dic[key],game,Upper,Lower,move_dic)
         turn+=1
+    
 #python -m search search/1v2.json
